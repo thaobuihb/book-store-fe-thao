3176b8f5222379eefec010482ff88af555bd162b
thaobuihb <thaohb@gmail.com>
2025-04-19 14:21:07 +0700

    update

--- src/features/book/Slideshow.js
+++ src/features/book/Slideshow.js
@@ -21,16 +21,15 @@ const SlideshowContainer = styled("div")(({ theme }) => ({
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
-  padding: theme.spacing(2),
   borderRadius: theme.shape.borderRadius,
   overflow: "hidden",
 
-  minHeight: 320,
+  height: 400, 
   [theme.breakpoints.down("md")]: {
-    minHeight: 280,
+    height: 300,
   },
   [theme.breakpoints.down("sm")]: {
-    minHeight: 240,
+    height: 260,
     padding: theme.spacing(1),
   },
 }));
@@ -44,65 +43,126 @@ const Slideshow = ({ books }) => {
 
   return (
     <SlideshowContainer>
-      <Grid container spacing={2} justifyContent="center">
-        {displayBooks.map((book) => (
-          <Grid item xs={10} sm={6} md={4} key={book._id}>
-            <Card
-              sx={{
-                width: isMobile ? 200 : 280,
-                height: isMobile ? 260 : 370,
-                m: 1,
-                p: 1,
-                boxShadow: 3,
-                borderRadius: 2,
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "space-between",
-                alignItems: "center",
-              }}
-            >
-              <CardActionArea onClick={() => navigate(`/book/${book._id}`)}>
-                <Box
+      {isMobile ? (
+        // ✅ Mobile: chỉ hiện 1 ảnh, không dùng Grid
+        displayBooks.map((book) => (
+          <Card
+            key={book._id}
+            sx={{
+              width: 200,
+              height: 250,
+              m: 1,
+              p: 1,
+              boxShadow: 3,
+              borderRadius: 2,
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <CardActionArea onClick={() => navigate(`/book/${book._id}`)}>
+              <Box
+                sx={{
+                  height: 160,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  p: 1,
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  image={book.img}
+                  alt="Book Cover"
                   sx={{
-                    height: isMobile ? 160 : 280,
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    p: 1,
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "contain",
+                    borderRadius: 1,
                   }}
-                >
-                  <CardMedia
-                    component="img"
-                    image={book.img}
-                    alt="Book Cover"
+                />
+              </Box>
+              <Typography
+                variant="body1"
+                align="center"
+                sx={{
+                  mt: 1,
+                  fontSize: 14,
+                  fontWeight: "bold",
+                  whiteSpace: "nowrap",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                  width: "90%",
+                }}
+              >
+                {book.name}
+              </Typography>
+            </CardActionArea>
+          </Card>
+        ))
+      ) : (
+        // ✅ Desktop: dùng Grid như cũ
+        <Grid container spacing={2} justifyContent="center">
+          {displayBooks.map((book) => (
+            <Grid item xs={10} sm={6} md={4} key={book._id}>
+              <Card
+                sx={{
+                  width: 280,
+                  height: 370,
+                  m: 1,
+                  p: 1,
+                  boxShadow: 3,
+                  borderRadius: 2,
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <CardActionArea onClick={() => navigate(`/book/${book._id}`)}>
+                  <Box
                     sx={{
-                      width: "100%",
-                      height: "100%",
-                      objectFit: "contain",
-                      borderRadius: 1,
+                      height: 280,
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      p: 1,
                     }}
-                  />
-                </Box>
-                <Typography
-                  variant="body1"
-                  align="center"
-                  sx={{
-                    mt: 1,
-                    fontSize: isMobile ? 14 : 18,
-                    fontWeight: "bold",
-                    whiteSpace: "nowrap",
-                    overflow: "hidden",
-                    textOverflow: "ellipsis",
-                    width: "90%",
-                  }}
-                >
-                  {book.name}
-                </Typography>
-              </CardActionArea>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
+                  >
+                    <CardMedia
+                      component="img"
+                      image={book.img}
+                      alt="Book Cover"
+                      sx={{
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "contain",
+                        borderRadius: 1,
+                      }}
+                    />
+                  </Box>
+                  <Typography
+                    variant="h6"
+                    align="center"
+                    sx={{
+                      mt: 1,
+                      fontSize: 18,
+                      fontWeight: "bold",
+                      whiteSpace: "nowrap",
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                      width: "90%",
+                    }}
+                  >
+                    {book.name}
+                  </Typography>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      )}
     </SlideshowContainer>
   );
 };
